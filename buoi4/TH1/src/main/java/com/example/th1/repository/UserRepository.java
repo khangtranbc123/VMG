83f175273865bfe6787717ecb97f213d90841aa7
khangtranbc123 <{t.v.khang202@gmail.com}>
2022-09-11 23:39:48 +0700

    add

---
+++ buoi4/TH1/src/main/java/com/example/th1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.th1.repository;
+
+import com.example.th1.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
